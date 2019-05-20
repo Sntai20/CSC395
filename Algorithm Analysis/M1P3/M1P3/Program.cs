804df7da2cce4b22acf9fb152dfeb288dd4c861d
Antonio Santana <47947504+Sntai20@users.noreply.github.com>
2019-05-19 22:18:06 -0700

    Completed M1P3

--- Algorithm Analysis/M1P3/M1P3/Program.cs
+++ Algorithm Analysis/M1P3/M1P3/Program.cs
@@ -1,7 +1,6 @@
-﻿// Ask the user to enter a positive integer b.
-// Validate the input(throw an exception if the input is not positive).
-// Then output whether or not the number b is divisible by 3.
-
+﻿// Read from contents from file(e.g. "Welcome to SMU!")
+// Pass file contents to CountVowels.
+// Then output the number of vowels found in the file.
 using System;
 
 namespace M1P3
@@ -10,47 +9,33 @@ namespace M1P3
     {
         public static void Main(string[] args)
         {
-            // Ask the user to enter a string (e.g. "Welcome to SMU!"
-            Console.Write("Please enter a string: ");
+            // Read from contents from file(e.g. "Welcome to SMU!")
+            Console.Write("Reading file ");
 
-            // Store user input in a variable.
-            //string userInput = Console.ReadLine();
-            string userInput = "Welcome to Saint Martin’s U!";
-            ReadFromFile();
-            Console.WriteLine(CountVowels(userInput));
-
+            // Pass file contents to CountVowels and print results.
+            Console.WriteLine(CountVowels(ReadFromFile()));
           
             Console.WriteLine("Press any key to exit.");
             System.Console.ReadKey();
         }
 
-        private static void ReadFromFile()
+        private static string ReadFromFile()
         {
-            // Example #2
-            // Read each line of the file into a string array. Each element
-            // of the array is one line of the file.
-            string[] lines = System.IO.File.ReadAllLines(@"C:\Users\Antonio\Documents\GitHub\CSC395\M1P3\M1P3.txt ");
-
-            // Display the file contents by using a foreach loop.
-            System.Console.WriteLine("Contents of M1P3.txt = ");
-            foreach (string line in lines)
-            {
-                // Use a tab to indent each line of the file.
-                Console.WriteLine("\t" + line);
-            }
+            // Read text from text file and store in string.
+            string text = System.IO.File.ReadAllText
+                (@"C:\Users\Antonio\Documents\GitHub\CSC395\Algorithm Analysis\M1P3\M1P3\M1P3.txt");
+         
+            // Pass the file contents to the CountVowels function.
+            return text;
         }
 
         public static int CountVowels(string userInput)
         {
             // Convert userInput to lowercase.
             string lowerCase = userInput.ToLower();
-            //Console.WriteLine(userInput.ToLower());
 
             // Covert to string to array and store in arr variable.
             char[] arr = lowerCase.ToCharArray();
-            //char ch = arr[];
-            //foreach (char c in arr)
-            //Console.WriteLine(c);
 
             // Find the vowels in userInput
             string vowelString = "aeiouy";
@@ -58,6 +43,7 @@ namespace M1P3
 
             int counter = 0;
             Console.WriteLine("\nSearching for vowels in this string");
+            // Compare each character against the vowels array.
             for (int i = 0; i < arr.Length; i++)
             {
                 //Console.WriteLine("Array = {0}", arr[i]);
