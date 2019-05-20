9409b40b2e9ed8cc4b76d53eeabb9b8eae0df908
Antonio Santana <antonio.santana.ir@outlook.com>
2019-05-19 18:39:09 -0700

    M1P1 Complete

--- Algorithm Analysis/M1P1/M1P1/Program.cs
+++ Algorithm Analysis/M1P1/M1P1/Program.cs
@@ -1,7 +1,7 @@
 ﻿// Ask the user to enter a string (e.g. "Welcome to SMU!"
 // The program should count and display the total number of vowels (A,E,I,O,U)
 // Count both uppercase and lowercase vowels in the given input. 
-// The ouput should be 9 in the example aboce.
+// The ouput should be 9 in the example above.
 using System;
 
 namespace M1P1
@@ -14,15 +14,40 @@ namespace M1P1
             Console.Write("Please enter a string: ");
 
             // Store user input in a variable.
-            string userInput = Console.ReadLine();
+            //string userInput = Console.ReadLine();
+            string userInput = "Welcome to a Saint Martin's University!";
+
 
             // Convert userInput to lowercase.
-            //userInput.ToLower();
-            Console.WriteLine(userInput.ToLower());
+            string lowerCase = userInput.ToLower();
+            //Console.WriteLine(userInput.ToLower());
 
-            // Count total number of vowels in userInput.
+            // Covert to string to array and store in arr variable.
+            char[] arr = lowerCase.ToCharArray();
+            //char ch = arr[];
+            //foreach (char c in arr)
+            //Console.WriteLine(c);
 
+            // Find the vowels in userInput
+            string vowelString = "aeiouy";
+            char[] vowels = vowelString.ToCharArray();
 
+            int counter = 0;
+            Console.WriteLine("\nSearching for vowels in this string");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                //Console.WriteLine("Array = {0}", arr[i]);
+                for (int j = 0; j < vowels.Length; j++)
+                {
+                    // Evaluate if the userInput contains vowels.
+                    if (arr[i] == vowels[j])
+                    {
+                        // Store the total number of vowels.
+                        counter = counter + 1;
+                    }
+                }
+            }
+            Console.WriteLine("{0} vowels counted in the string", counter);
         }
     }
 }
