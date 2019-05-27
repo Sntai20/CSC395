5cf91ec46e21ec2c76f4215fb1d075686d180d57
Antonio Santana <antonio.santana.ir@outlook.com>
2019-05-27 12:21:39 -0700

    Complete.

--- Search and Sort Algorithms/CSC395 Module2 Sorting/CSC395 Module2 Sorting/Program.cs
+++ Search and Sort Algorithms/CSC395 Module2 Sorting/CSC395 Module2 Sorting/Program.cs
@@ -9,52 +9,56 @@ namespace CSC395_Module2_Sorting
 {
     class Program
     {
-        //TODO write the running time of the method.
+        // O(n^2) running time of the method.
         static void Main(string[] args)
         {
             // Read user input.
-            string path = @"/Users/antonio/Library/Mobile Documents/com~apple~CloudDocs/Documents/Education and Training/SMU/CSC395/M2 Homework/input.txt";
+            string path = @"input2.txt";
 
-            //TODO 
-            //Read the entries from input.txt (one line per entry) and store them into an array.
-            //Four array copies, each copy call bubbleReverseSort, selectionReverseSort, 
-            //mergeReverseSort, quickReverseSort. 
+            // Read the entries from input.txt (one line per entry) and store them into an array.
+            // Each copy call bubbleReverseSort, selectionReverseSort, mergeReverseSort, quickReverseSort. 
             string[] myArr = Utility.ReadFromFile(path);
             string[] myArr1 = Utility.ReadFromFile(path);
             string[] myArr2 = Utility.ReadFromFile(path);
             string[] myArr3 = Utility.ReadFromFile(path);
             string[] myArr4 = Utility.ReadFromFile(path);
 
-            //TODO For each of these measure the execution time
-            //(how long it took to run the reverse sorting) and display this time.
+            // Each algorthim's execution time is measured and displayed.
+            // var watch* = System.Diagnostics.Stopwatch.StartNew();
+            // watch*.Stop();
+            // var elapsedMs1 = watch*.ElapsedMilliseconds;
 
             // Sort user input.
-            //BubbleReverseSort performed 108812539 comparisons.
-            //BubbleReverseSort: Running time: 97599 milliseconds.
+            // BubbleReverseSort performed 108812539 comparisons.
+            // BubbleReverseSort: Running time: 97599 milliseconds.
+            // O(n^2)
             var watch1 = System.Diagnostics.Stopwatch.StartNew();
             Utility.BubbleReverseSort(myArr1);
             watch1.Stop();
             var elapsedMs1 = watch1.ElapsedMilliseconds;
             Console.WriteLine($"BubbleReverseSort: Running time: {elapsedMs1} milliseconds.");
 
-            //SelectionReverseSort performed 20843 comparisons.
-            //SelectionReverseSort: Running time: 64338 milliseconds.
+            // SelectionReverseSort performed 20843 comparisons.
+            // SelectionReverseSort: Running time: 64338 milliseconds.
+            // O(n^2)
             var watch2 = System.Diagnostics.Stopwatch.StartNew();
             Utility.SelectionReverseSort(myArr2);
             watch2.Stop();
             var elapsedMs2 = watch2.ElapsedMilliseconds;
             Console.WriteLine($"SelectionReverseSort: Running time: {elapsedMs2} milliseconds.");
 
-            //MergeReverseSort performed 273179 comparisons.
-            //MergeReverseSort: Running time: 238 milliseconds.
+            // MergeReverseSort performed 273179 comparisons.
+            // MergeReverseSort: Running time: 201 milliseconds.
+            // O(n log n)
             var watch3 = System.Diagnostics.Stopwatch.StartNew();
             Utility.MergeReverseSort(myArr3);
             watch3.Stop();
             var elapsedMs3 = watch3.ElapsedMilliseconds;
             Console.WriteLine($"MergeReverseSort: Running time: {elapsedMs3} milliseconds.");
 
-            //QuickReverseSort performed 335080 comparisons.
-            //QuickReverseSort: Running time: 267 milliseconds.
+            // QuickReverseSort performed 335080 comparisons.
+            // QuickReverseSort: Running time: 188 milliseconds.
+            // O(n log n)
             var watch4 = System.Diagnostics.Stopwatch.StartNew();
             Utility.QuickReverseSort(myArr4);
             watch4.Stop();
--- Search and Sort Algorithms/CSC395 Module2 Sorting/CSC395 Module2 Sorting/Utility.cs
+++ Search and Sort Algorithms/CSC395 Module2 Sorting/CSC395 Module2 Sorting/Utility.cs
@@ -9,48 +9,49 @@ namespace CSC395_Module2_Sorting
 {
     class Utility
     {
-        //TODO write the running time of the method.
-        //Modify the merge sort algorithm seen in class so it works with an array 
-        //of strings AND the array will have the values sorted in reverse. Also 
-        //add a local variable count(use long count) of the number of comparisons 
-        //that were performed.Display it before exiting this method.
+        // Merge sort algorithm sorts an array of strings in reverse.
+        // A local variable count, type long, stores the number of comparisons 
+        // performed and displays the value before exiting the method.
         public static void MergeReverseSort(string[] myArr3)
         {
-            double count = 0; 
-            string[] tmp = new string[myArr3.Length]; //used as a temp buffer
-            MergeReverseSortHelper(myArr3, 0, myArr3.Length - 1, tmp, ref count);
+            long count = 0;
+            if (myArr3 != null)
+            {
+                // tmp is used as a temp buffer and passed to the helper.
+                string[] tmp = new string[myArr3.Length]; 
+                MergeReverseSortHelper(myArr3, 0, myArr3.Length - 1, tmp, ref count);
+            }
             Console.WriteLine($"\nMergeReverseSort performed {count} comparisons.");
         }
 
-        public static void MergeReverseSortHelper(string[] myArr3, int first, int last, string[] tmp, ref double count)
+        public static void MergeReverseSortHelper(string[] myArr3, int first, int last, string[] tmp, ref long count)
         {
-            //double count = 0;
-            if (first < last)//if we have at least two elements
+            // Validate there are at least two elements.
+            if (first < last)
             {
+                // Divide the array in two recursively.
                 int mid = (first + last) / 2;
                 MergeReverseSortHelper(myArr3, first, mid, tmp, ref count);
                 MergeReverseSortHelper(myArr3, mid + 1, last, tmp, ref count);
                 Merge(myArr3, first, mid + 1, last, tmp, ref count);
             }
-            //Console.WriteLine($"\nMergeReverseSort performed {count} comparisons.");
         }
 
-        public static void Merge(string[] myArr3, int first, int mid, int last, string[] tmp, ref double count)
+        public static void Merge(string[] myArr3, int first, int mid, int last, string[] tmp, ref long count)
         {
-            //tmp is the temporary buffer
+            // tmp is passed in to be used as a temporary buffer.
             int i = first;
             int j = mid;
             int k = first;
 
             while (i<mid && j <=last )
             {
+                // Compare myArr[i] to myArr[j] with currentculture rules and choose the larger.
                 if (string.Compare(myArr3[i], myArr3[j], StringComparison.CurrentCulture) > 0)
-                    //if (arr[i] < arr[j])//pick the smallest of the two ...
                 {
                     tmp[k] = myArr3[i];
                     k++;
                     i++;
-
                 }
                 else
                 {
@@ -61,8 +62,8 @@ namespace CSC395_Module2_Sorting
                 count++;
             }
 
-            //put the remaining elements
-            while(i<mid) //only runs if there are elements left in the first half
+            // Put the remaining elements and only runs if there are elements left in the first half.
+            while (i<mid) 
             {
                 tmp[k] = myArr3[i];
                 k++;
@@ -76,218 +77,150 @@ namespace CSC395_Module2_Sorting
                 k++;
             }
 
-            //copy back tmp into arr
+            // Copy the tmp value back into the array.
             for (int p = first; p <= last; p++)
                 myArr3[p] = tmp[p];
         }
 
-        //TODO write the running time of the method.
-        //Modify the quick sort algorithm seen in class so it works with an 
-        //array of strings AND the array will have the values sorted in reverse. 
-        //Also add a local variable count(use long count) of the number of 
-        //comparisons that were performed.Display it before exiting this method.
+        // Quick sort algorithm sorts an array of strings in reverse.
+        // A local variable count, type long, stores the number of comparisons 
+        // performed and displays the value before exiting the method.
         public static void QuickReverseSort(string[] myArr4)
         {
-            double count = 0;
+            long count = 0;
             if (myArr4 != null)
             {
-                quickSortHelper(myArr4, 0, myArr4.Length - 1, ref count);
+                QuickReverseSortHelper(myArr4, 0, myArr4.Length - 1, ref count);
             }
             Console.WriteLine($"\nQuickReverseSort performed {count} comparisons.");
         }
 
-        public static void quickSortHelper(string[] myArr4, int left, int right, ref double count)
+        public static void QuickReverseSortHelper(string[] myArr4, int left, int right, ref long count)
         {
+            // Validate the sorting stays within the array.
             if (left < right)
             {
+                // Keep track of the partition index and recursively sort array.
                 int partIdx = Partition(myArr4, left, right, ref count);
-                quickSortHelper(myArr4, left, partIdx - 1, ref count);
-                quickSortHelper(myArr4, partIdx + 1, right, ref count);
+                QuickReverseSortHelper(myArr4, left, partIdx - 1, ref count);
+                QuickReverseSortHelper(myArr4, partIdx + 1, right, ref count);
             }
         }
 
-        static public int Partition(string[] myArr4, int left, int right, ref double count)
+        static public int Partition(string[] myArr4, int left, int right, ref long count)
         {
-            string pivot = myArr4[right];
-            int i = (left - 1);
+            int lessIndex = (left - 1);
+            string tmp;
+            string tmp1;
+
+            // Compares elements starting from the left to the end of the array.
             for (int j = left; j < right; j++)
             {
-                if (string.Compare(myArr4[j], pivot, StringComparison.CurrentCulture) > 0)
+                // Compare myArr[j] to myArr[right] with currentculture rules.
+                if (string.Compare(myArr4[j], myArr4[right], StringComparison.CurrentCulture) > 0)
                 {
-                    i++;
-                    string tmp = myArr4[i];
-                    myArr4[i] = myArr4[j];
+                    // Swap lessIndex with tmp.
+                    lessIndex++;
+                    tmp = myArr4[lessIndex];
+                    myArr4[lessIndex] = myArr4[j];
                     myArr4[j] = tmp;
-
+                    count++;
                 }
-                count++;
+
             }
 
-            string tmp1 = myArr4[i + 1];
-            myArr4[i + 1] = myArr4[right];
+            // Swap lessIndex with right.
+            tmp1 = myArr4[lessIndex + 1];
+            myArr4[lessIndex + 1] = myArr4[right];
             myArr4[right] = tmp1;
-            return i + 1;
+            return lessIndex + 1;
         }
 
-        public static void InsertionSort(int[] arr)//worst case:  O(n^2), best case is Omega(n)
+        // Selection sort algorithm sorts an array of strings in reverse.
+        // A local variable count, type long, stores the number of comparisons 
+        // performed and displays the value before exiting the method.
+        public static void SelectionReverseSort(string[] myArr2)
+        {
+            long count = 0;
+            string tmp;
+            if (myArr2 != null)
             {
-                for(int i=1;i<arr.Length;i++)
+                for (int i = 0; i < myArr2.Length - 1; i++)
                 {
-                    int tmp = arr[i];//save the current element
-                    //look to the left and shift all elements larger than tmp to the right, one position
-                    int j = i - 1;
-                    while (j >= 0 && arr[j]>tmp)
+                    int minPos = i;
+
+                    for (int j = i + 1; j < myArr2.Length; j++)
                     {
-                        arr[j + 1] = arr[j];
-                        j--;
+                        // Compare myArr[j] to myArr[minPos] with currentculture rules and choose the larger.
+                        if (string.Compare(myArr2[j], myArr2[minPos], StringComparison.CurrentCulture) > 0)
+                        {
+                            minPos = j;
+                            count++;
+                        }
                     }
 
-                    //put tmp back into the arr
-                    arr[j+1] = tmp;
-                }
-            }
-
-        //TODO write the running time of the method.
-        //Modify the selection sort algorithm seen in class so it works with an 
-        //array of strings AND the array will have the values sorted in reverse.
-        //Also add a local variable count(use long count) of the number of 
-        //comparisons that were performed.Display it before exiting this method.
-        public static void SelectionReverseSort(string[] myArr)
-        {
-            double count = 0;
-
-            for (int i = 0; i < myArr.Length - 1; i++)
-            {
-                int minPos = i;
-
-                for (int j = i + 1; j < myArr.Length; j++)
-                    if (string.Compare(myArr[j], myArr[minPos], StringComparison.CurrentCulture) > 0)
-                        minPos = j;
                     //swap the elements at minPos and i
-                    string tmp = myArr[i];
-                    myArr[i] = myArr[minPos];
-                    myArr[minPos] = tmp;
-                    count++;
-
-                //if (myArr.Length > minPos)
-                //if (string.Compare(myArr[i], myArr[i + 1], StringComparison.CurrentCulture) <= 0)
-            }
-            Console.WriteLine($"\nSelectionReverseSort performed {count} comparisons.");
-        }
-
-        public static void SelectionSort(int[] arr) //best and worst is O(n^2)
-        {
-            for(int i=0; i<arr.Length-1;i++)
-            {
-                int minPos = i;
-                for(int j=i+1;j<arr.Length;j++)
-                    if (arr[j] < arr[minPos])
-                        minPos = j;
-                
-                //swap the elements at minPos and i
-                int tmp = arr[i];
-                arr[i] = arr[minPos];
-                arr[minPos] = tmp;
+                    tmp = myArr2[i];
+                    myArr2[i] = myArr2[minPos];
+                    myArr2[minPos] = tmp;
+                }
+                Console.WriteLine($"\nSelectionReverseSort performed {count} comparisons.");
             }
         }
 
-        //TODO write the running time of the method.
-        //Added a bubble sort algorithm to sort an array of strings in reverse. 
-        //Added a local variable to count of the number of comparisons that were performed.
-        //Display counter before exiting this method.
-        public static void BubbleReverseSort(string[] myArr)
+        // Bubble sort algorithm sorts an array of strings in reverse.
+        // A local variable count, type long, stores the number of comparisons 
+        // performed and displays the value before exiting the method.
+        // Worst case: O(n^2), best case is Omega(n)
+        public static void BubbleReverseSort(string[] myArr1)
         {
             string tmp;
-            bool flag = false;
+            bool flag;
             long count = 0;
 
-            for (int j = myArr.Length - 1; j > 0; j--)
+            if (myArr1 != null)
             {
-                flag = false;
-                for (int i = 0; i < j; i++)
-                    //if (myArr[i].CompareTo(myArr[i+1])
-                    if (string.Compare(myArr[i], myArr[i + 1], StringComparison.CurrentCulture) < 0)
+                // Compares elements starting from the end of the array.
+                for (int j = myArr1.Length - 1; j > 0; j--)
+                {
+                    flag = false;
+                    for (int i = 0; i < j; i++)
                     {
-                        tmp = myArr[i];
-                        myArr[i] = myArr[i + 1];
-                        myArr[i + 1] = tmp;
-                        flag = true;
-                        count++;
+                        // Compare myArr[i] to myArr[i+1] with currentculture rules.
+                        if (string.Compare(myArr1[i], myArr1[i + 1], StringComparison.CurrentCulture) < 0)
+                        {
+                            // Swap indices and add one to count.
+                            tmp = myArr1[i];
+                            myArr1[i] = myArr1[i + 1];
+                            myArr1[i + 1] = tmp;
+                            flag = true;
+                            count++;
+                        }
                     }
-                if (flag == false)//i.e. the array is sorted
-                    break;
+                    // Check if array is sorted after each iteration.
+                    if (flag == false)
+                        break;
+                }
             }
             Console.WriteLine($"\nBubbleReverseSort performed {count} comparisons.");
         }
 
-        public static void BubbleSort(int[] arr) // worst case: O(n^2), best case: Omega(n^2) => Theta(n^2)
-        {
-            int tmp;
-            for (int j = arr.Length - 1; j>0;j--)
-            {
-                for (int i = 0; i < j; i++)
-                    if (arr[i] > arr[i + 1])
-                    {
-                        tmp = arr[i];
-                        arr[i] = arr[i + 1];
-                        arr[i + 1] = tmp;
-                    }
-            }
-        }
-
-        public static void BubbleSortImproved(int[] arr) //worst case:  O(n^2), best case is Omega(n)
-        {
-            int tmp;
-            bool flag = false;
-
-
-            for (int j = arr.Length - 1; j > 0; j--)
-            {
-                flag = false;
-                for (int i = 0; i < j; i++)
-                    if (arr[i] > arr[i + 1])
-                    {
-                        tmp = arr[i];
-                        arr[i] = arr[i + 1];
-                        arr[i + 1] = tmp;
-                        flag = true;
-                    }
-                if (flag == false)//i.e. the array is sorted
-                    break;
-            }
-        }
-        //public static void RunAll(string[] myArr1, string[] myArr2, string[] myArr3, string[] myArr4)
-        //{
-        //    ArrayList methods = new ArrayList
-            
-        //    };
-        //    //string[][] allArr = { myArr1, myArr2, myArr3, myArr4 };
-        //    foreach (var method in methods)
-        //    {
-        //        Console.Write(1++);
-        //        // To make the loop easier use var instead of int.
-        //        //foreach (var arrValue in myArr)
-
-        //        //Console.WriteLine();
-        //    }
-        //}
-
-        //TODO write the running time of the method.
         public static string[] ReadFromFile(string path)
         {
-            // Read text from text file and store in string.
-            string[] words = System.IO.File.ReadAllLines(path);
-
-            // Create parallel array of words by calling ToLowerInvariant.
-            string[] lowerWords = new string[words.Length];
-            for (int ctr = words.GetLowerBound(0); ctr <= words.GetUpperBound(0); ctr++)
-                lowerWords[ctr] = words[ctr].ToLower();
-
-            //foreach (string lowerWord in lowerWords)
+            // Read text from text file, change to lower, and store in lowerWords.
+            //try
             //{
-            //    Console.Write(lowerWord + " ");
+                string[] words = System.IO.File.ReadAllLines(path);
+                // Create parallel array of words by calling ToLower.
+                string[] lowerWords = new string[words.Length];
+                for (int ctr = words.GetLowerBound(0); ctr <= words.GetUpperBound(0); ctr++)
+                    lowerWords[ctr] = words[ctr].ToLower();
             //}
-            // Pass the file contents to the CountVowels function.
+            //catch(System.IO.DirectoryNotFoundException)
+            //{
+                //Console.WriteLine($"The following file path could not be found: {path}");  
+            //}
+            // Pass the file contents to sorting algorthims.
             return lowerWords;
         }
 
