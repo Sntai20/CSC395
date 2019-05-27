e6a07800f8deeb8525283823dc63680e939ef464
Antonio Santana <antonio.santana.ir@outlook.com>
2019-05-27 12:31:09 -0700

    Class examples.

---
+++ Class Examples/CSC395 Module2 Sorting/CSC395 Module2 Sorting/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC395_Module2_Sorting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] myArr = { 21, 107, 6, 50,  6, 100, 3};
+
+            //display the array values before sorting
+            Console.WriteLine("The array values BEFORE sorting: ");
+            foreach(int num in myArr)
+                Console.Write(num+" ");
+            Console.WriteLine();
+
+            //Utility.BubbleSortImproved(myArr);//sort the array
+            //Utility.SelectionSort(myArr);
+            //Utility.InsertionSort(myArr);
+            //Utility.MergeSort(myArr);
+            //Utility.QuickSort(myArr);
+            Array.Sort(myArr);
+
+            //display the array values after sorting
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.WriteLine("The array values AFTER sorting: ");
+            foreach (int num in myArr)
+                Console.Write(num + " ");
+            Console.WriteLine();
+
+
+            Student[] myArr2 = {    new Student("Alice", "CS"),
+                                    new Student("Chad", "Pre-law"),
+                                    new Student("Bob", "Dance"),
+                                    new Student("Sam", "EE")};
+            Array.Sort(myArr2, (st1, st2) => String.Compare(st1.Major,st2.Major));
+
+            foreach(var st in myArr2)
+                Console.WriteLine(st);
+        }
+    }
+
+    class Student
+    {
+        public string Name { get; set; }
+        public string Major { get; set; }
+
+        public Student(string name, string Major)
+        {
+            Name = name;
+            this.Major = Major;
+        }
+
+        public override string ToString()
+        {
+            return Name + " : " + Major;
+        }
+    }
+}
---
+++ Class Examples/CSC395 Module3/CSC395 Module3/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC395_Module3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create a new node
+            Node myNode = new Node(0);
+
+            SinglyLinkedList myList = new SinglyLinkedList();
+            Console.WriteLine(myList.IsEmpty());
+            myList.AddLast(7);
+
+            myList.PrintList();
+
+            myList.AddFirst(1);
+            myList.AddFirst(2);
+            myList.AddFirst(3);
+            myList.PrintList();
+
+            Console.WriteLine();
+            myList.AddLast(10);
+            myList.AddLast(20);
+            myList.AddLast(30);
+            myList.PrintList();
+        }
+    }
+
+    class Node
+    {
+        //data
+        public int value { get; set; }
+        public Node next;
+
+        //methods
+
+        //ctor
+        public Node(int value)
+        {
+            this.value = value;
+            this.next = null;
+        }
+
+    }
+
+    class SinglyLinkedList
+    {
+        //data
+        Node first;
+
+        //methods
+        public bool IsEmpty()
+        {
+            //if (first == null)
+            //    return true;
+            //else
+            //    return false;
+            return first == null;
+        }
+
+        public void AddLast(int val)
+        {
+            if (IsEmpty())
+                AddFirst(val);
+            else
+            {
+                Node newNode = new Node(val);
+
+                Node current = first;
+                while (current.next != null)
+                {
+                    current = current.next;
+                }
+
+                current.next = newNode;
+            }
+        }
+
+
+        public void AddFirst(int value)
+        {
+            Node newNode = new Node(value);
+            newNode.next = first;
+            first = newNode;
+        }
+
+        public void PrintList()
+        {
+            Console.WriteLine();
+            Console.WriteLine();
+            if(IsEmpty())
+                Console.WriteLine("the list is empty!!!");
+            else
+            {
+                Node current = first;
+                while (current != null)
+                {
+                    Console.Write(current.value + "  ");
+                    current = current.next;
+                }
+            }
+        }
+        //ctor
+        public SinglyLinkedList()
+        {
+            first = null;
+        }
+    }
+}
