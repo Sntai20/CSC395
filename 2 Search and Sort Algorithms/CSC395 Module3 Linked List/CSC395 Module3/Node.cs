f1b3ce1b7325efe5acab2db0dd0801ae6d8b7652
Antonio Santana <antonio.santana.ir@outlook.com>
2019-06-02 12:44:39 -0700

    Created new branch and add comments to node class.

--- 2 Search and Sort Algorithms/CSC395 Module3 Linked List/CSC395 Module3/Node.cs
+++ 2 Search and Sort Algorithms/CSC395 Module3 Linked List/CSC395 Module3/Node.cs
@@ -3,13 +3,13 @@ namespace CSC395_Module3
 {
     public class Node
     {
-        //data
+        // data
         public string value { get; set; }
         public Node next;
 
         //methods
 
-        //ctor
+        // Constructor to initialize the value in the node and point to null.
         public Node(string value)
         {
             this.value = value;
