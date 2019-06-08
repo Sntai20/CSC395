0f11591ea5119b6811afeefdb11ea1753b5620c9
Antonio Santana <antonio.santana.ir@outlook.com>
2019-06-08 12:26:04 -0700

    Organized file.

---
+++ 2 Search and Sort Algorithms/CSC395 Module3 Queue/CSC395 Module3/Node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace CSC395_Module3
+{
+    class Node
+    {
+        //data
+        public int value { get; set; }
+        public Node next;
+
+        //methods
+
+        //ctor
+        public Node(int value)
+        {
+            this.value = value;
+            this.next = null;
+        }
+
+    }
+}
---
+++ 2 Search and Sort Algorithms/CSC395 Module3 Queue/CSC395 Module3/Queue2.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace CSC395_Module3
+{
+    public class Queue2
+    {
+        public Queue2()
+        {
+        }
+    }
+}
