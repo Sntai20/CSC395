c864d9a18b59f65b94d18711c5b75b7e2251c281
Antonio Santana <antonio.santana.ir@outlook.com>
2019-06-22 11:27:07 -0700

    Added CS395M5 Graph

---
+++ 2 Search and Sort Algorithms/CSC395 Module5 Graph/CSC395 Module5/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSC395_Module5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Graph myGraph = new Graph();
+
+            myGraph.AddVertex("Alice");
+            myGraph.AddVertex("Bob");
+            myGraph.AddVertex("Carl");
+
+
+
+            myGraph.AddEdge("Alice", "Carl", 2019);
+            myGraph.AddEdge("Alice", "Alice");
+
+            myGraph.PrintVertices();
+            Console.WriteLine();
+            myGraph.PrintEdges2();
+
+
+
+            myGraph.RemoveVertex("Carl");
+
+
+            myGraph.PrintVertices();
+            Console.WriteLine();
+            myGraph.PrintEdges2();
+        }
+    }
+}
